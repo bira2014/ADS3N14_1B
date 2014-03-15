1d3f6ca51dc953d2d0346a01676409433e0a6b3e
bira2014 <birasan_@hotmail.com>
2014-03-15 16:14:24 -0300

    Criado o Projeto Pessoa

---
+++ HelloWorld/src/HelloYou.java
@@ -0,0 +1,17 @@
+/* Primeiro Programa Criado
+/ Prof Rafael Jeffman  - Algoritimo III
+/4. Alterar o projeto(HelloWord) 
+/ * e inserir as modificações 
+/(Hello, You) no repositório.*/
+
+import static java.lang.System.out;
+public class HelloYou {
+
+	
+	public static void main(String[] args) {
+	
+	   System.out.println("Ola");
+	   System.out.println("Meu Nome é : Bira");
+	}
+
+}
